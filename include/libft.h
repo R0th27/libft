76bca2ea172dff7d9b3408f7e3eca81b64a119e1
William <htetwaiyantoe.mmr@gmail.com>
2026-01-14 14:36:43 +0700

    modification on ft_strncmp

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: htoe <htoe@student.42bangkok.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 21:54:23 by htoe              #+#    #+#             */
-/*   Updated: 2026/01/14 14:07:00 by htoe             ###   ########.fr       */
+/*   Updated: 2026/01/14 14:20:57 by htoe             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 # define LIBFT_H
 
 //standard headers
-#include <stdlib.h>
-#include <unistd.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-//LIBC FUNCTIONS
-//type
+//LIBC FUNCTIONS * 23
+//type * 5
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
 
-//strings
+//strings * 9
 size_t	ft_strlen(const char *s);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
@@ -36,16 +36,15 @@ size_t	ft_strlcat(char *dest, const char *src, size_t dsize);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 
-//search and compare
+//search and compare * 6
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-char	*ft_strnstr(const char *str, const char *sub, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 
-//conversion
+//conversion * 3
 int		ft_atoi(const char *nptr);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
